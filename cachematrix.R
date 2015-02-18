7a757c9504735c5e84f69131d67d51038904792a
Terka <bilteresa@gmail.com>
2015-02-18 21:57:14 +0200

    Filling up both functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Those two functions are designed to ofload CPU. 
+## To decreas the time required to obtain desired result, in this case inverse of matrix
 
-## Write a short comment describing this function
-
+## This function is creating a special list
 makeCacheMatrix <- function(x = matrix()) {
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setmean <- function(solve) s <<- solve
+        getmean <- function() s
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 
 }
 
 
-## Write a short comment describing this function
+## This function is calculating the invers of function. But firstly is checking if 
+## inverse was solved before. If yes function doesn't need to do calculation again
+## just assign the catched value to result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
